69f6ff8f039982967008ff2db7d9c8e9dcece441
viskrishnan <krishnansubramanian007@gmail.com>
2024-08-20 14:30:02 +0530

    Implemented a simple Project: Countdown App

---
+++ time-till-deadline.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+user_input = input("Enter your goal deadline separated by colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+deadline_date = datetime.strptime(deadline,"%d.%m.%Y")
+# how many days from now is the deadline
+
+today_date = datetime.today()
+
+time_till = deadline_date - today_date
+hours_till = int(time_till.total_seconds() / 60 / 60)
+print(f"Time remaining for learning goal is : {goal} is {hours_till} hours")
+
+
+
